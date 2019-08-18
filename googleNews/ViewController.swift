02baccd8bae47cab833fb5235622661460fefc05
Станислава <stanislava@macbook-air-stanislava.local>
2019-08-18 21:32:20 +0300

    Initial Commit

---
+++ googleNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  googleNews
+//
+//  Created by Станислава on 18.08.2019.
+//  Copyright © 2019 Stminchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
