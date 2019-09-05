a94f711a9c0420924138a2879c8304f2f3783367
Станислава <stanislava@MacBook-Air-Stanislava.local>
2019-09-05 13:30:15 +0300

    Pum pum

---
+++ googleNews/News.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  googleNews
+//
+//  Created by Станислава on 18.08.2019.
+//  Copyright © 2019 Stminchuk. All rights reserved.
+//
+
+import Foundation
+
+struct Articles: Codable {
+    var articles: [PieceOfNews]
+}
---
+++ googleNews/PieceOfNews .swift
@@ -0,0 +1,9 @@
+//
+//  PieceOfNews .swift
+//  googleNews
+//
+//  Created by Станислава on 18.08.2019.
+//  Copyright © 2019 Stminchuk. All rights reserved.
+//
+
+import Foundation
